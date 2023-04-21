e0fb42b447ccf9565523030d4fbabaf9de4a8062
MarcosDev1982 <alan325_batera@hotmail.com>
2023-04-21 14:35:18 -0300

    Tratando BindException ao enviar parâmetros de URL inválidos

--- src/main/java/com/food/marcosfood/ipi/exceptionhander/ApiExcepetionHandler.java
+++ src/main/java/com/food/marcosfood/ipi/exceptionhander/ApiExcepetionHandler.java
@@ -22,6 +22,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -45,13 +46,24 @@ public class ApiExcepetionHandler extends ResponseEntityExceptionHandler {
     private MessageSource messageSource;
 
     @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        return handleValidationInternal(ex, headers, status, request, ex.getBindingResult());
+    }
+
+    @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request) {
 
+        return handleValidationInternal(ex, headers, status, request, ex.getBindingResult());
+    }
+
+    private ResponseEntity<Object> handleValidationInternal
+            (Exception ex, HttpHeaders headers, HttpStatus status, WebRequest request,  BindingResult bindingResult ) {
         ProblemaType problemType = ProblemaType.DADOS_INVALIDOS;
         String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
 
-        BindingResult bindingResult = ex.getBindingResult();
+
 
         List<Problema.Object> problemObject = bindingResult.getAllErrors().stream()
                 .map(objectError -> {
