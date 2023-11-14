7d74edebae6601936f4f8e0c619ccfedadaedd74
MarcosDev1982 <alan325_batera@hotmail.com>
2023-11-14 17:37:29 -0300

    Descrevendo parâmetros de entrada na documentação

--- src/main/java/com/food/marcosfood/api/contoller/CidadeController.java
+++ src/main/java/com/food/marcosfood/api/contoller/CidadeController.java
@@ -1,14 +1,17 @@
 package com.food.marcosfood.api.contoller;
 
+import com.food.marcosfood.api.model.CidadeDTO;
+import com.food.marcosfood.api.model.input.CidadeInput;
+import com.food.marcosfood.api.model.input.assembler.CidadeModelAssembler;
+import com.food.marcosfood.api.model.input.assembler.CidadeModelDesAssembler;
 import com.food.marcosfood.domain.exception.CozinhaNaoEncontadaException;
 import com.food.marcosfood.domain.exception.EstadoNaoEncotradoException;
 import com.food.marcosfood.domain.exception.NegocioExcepetion;
 import com.food.marcosfood.domain.model.Cidade;
-import com.food.marcosfood.api.model.input.CidadeInput;
 import com.food.marcosfood.domain.service.CidadeService;
-import com.food.marcosfood.api.model.input.assembler.CidadeModelAssembler;
-import com.food.marcosfood.api.model.input.assembler.CidadeModelDesAssembler;
-import com.food.marcosfood.api.model.CidadeDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = "Cidades")
 @RestController
 @RequestMapping("/cidades")
 public class CidadeController {
@@ -29,14 +33,16 @@ public class CidadeController {
     @Autowired
     private CidadeModelDesAssembler cidadeModelDesAssembler;
 
+    @ApiOperation("Lista Cidades")
     @GetMapping
     public List<CidadeDTO> findAll() {
 
         return cidadeModelAssembler.toCollectionModel(cidadeService.findAllCidade());
     }
 
+    @ApiOperation("Busca Cidade por Id")
     @GetMapping("/{cidadeId}")
-    public CidadeDTO findAllById(@PathVariable Long cidadeId) {
+    public CidadeDTO findAllById(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long cidadeId) {
         try {
 
             return cidadeModelAssembler.toModell(cidadeService.findByIdCidade(cidadeId));
@@ -46,10 +52,10 @@ public class CidadeController {
         }
 
     }
-
+    @ApiOperation("Cadastra uma cidade")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CidadeDTO create(@RequestBody @Valid CidadeInput cidadeInput) {
+    public CidadeDTO create(@ApiParam(name = "corpo", value = "Representação de uma nova cidade")@RequestBody @Valid CidadeInput cidadeInput) {
         try {
             Cidade cidade = cidadeModelDesAssembler.toDomainObejct(cidadeInput);
             return cidadeModelAssembler.toModell(cidadeService.cadastraCidade(cidade));
@@ -60,9 +66,9 @@ public class CidadeController {
         }
 
     }
-
+    @ApiOperation("Altera uma cidade")
     @PutMapping("/{cidadeId}")
-    public CidadeDTO alterCidade(@PathVariable Long cidadeId, @RequestBody @Valid CidadeInput cidadeInput) {
+    public CidadeDTO alterCidade(@ApiParam(value = "ID de um caidade", example = "1") @PathVariable Long cidadeId, @ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados") @RequestBody @Valid CidadeInput cidadeInput) {
 
 
         Cidade cidadeAtual = cidadeService.findByIdCidade(cidadeId);
@@ -71,9 +77,9 @@ public class CidadeController {
 
 
     }
-
+    @ApiOperation("Exclui uma cidade")
     @DeleteMapping("/{cidadeId}")
-    public void delete(@PathVariable Long cidadeId) {
+    public void delete(@ApiParam(value = "ID de um caidade", example = "1")@PathVariable Long cidadeId) {
 
         cidadeService.remover(cidadeId);
 
